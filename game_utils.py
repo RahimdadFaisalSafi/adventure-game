a25545b465fca5b4013a9adfa0bac7763bbc2e2a
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2024-10-07 16:33:49 +0200

    Die Funktion end_game wird implementiert.

--- game_utils.py
+++ game_utils.py
@@ -1,6 +1,7 @@
 PUZZLES = ['Ich habe eine Seite, aber keinen Rand. Ich kann sprechen, \
     aber keinen Mund. Ich kann fliegen, aber habe keine Flügel. Ich\
         kann gefallen, aber bin kein Regen.', ]
+
 def solve_riddle(riddle):
     print(f"Rätsel: {riddle}")
     answer = input("Deine Antwort: ").lower()
@@ -9,5 +10,8 @@ def solve_riddle(riddle):
     else:
         return False
 
+# Funktion zum Beenden des Spiels
 def end_game():
+    print("Vielen Dank, dass du 'Die verlorene Schatzsuche' gespielt hast!")
+    print("Bis zum nächsten Mal.")
     exit()
